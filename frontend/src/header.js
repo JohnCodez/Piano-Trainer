8c3eaf48265b69e21c6e6a90c7f447bae922cbcc
John <exarhouleasjohn@gmail.com>
2021-04-13 15:28:12 -0400

    Responsive looking header

---
+++ frontend/src/header.js
@@ -0,0 +1,15 @@
+login.addEventListener( 'mouseenter' , () => {
+    login.style.color = 'grey';
+    login.style.cursor = "pointer"
+})
+login.addEventListener( 'mouseleave' , () => {
+    login.style.color = 'white'
+})
+
+home.addEventListener( 'mouseenter' , () => {
+    home.style.color = 'grey';
+    home.style.cursor = "pointer"
+})
+home.addEventListener( 'mouseleave' , () => {
+    home.style.color = 'white'
+})
